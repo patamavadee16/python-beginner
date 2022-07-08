4c6d66aeccb4f774316321e74e9390210f72bd2b
Patamavadee Tubthong <pear40486patama@gmail.com>
2022-07-08 23:01:40 +0700

    update

---
+++ 01/print.py
@@ -0,0 +1,15 @@
+#String ใช้ ' '' '''
+print('''triple code''')
+print("double code")
+print('single code')
+#int float ระบุเลย
+print(-1)
+print(1)
+print(0)
+print(-1.1)
+#boolean
+print(True)
+print(False)
+#\t Tab \n ขึ้นบรรทัดใหม่
+print("Tab \t" + "ข้อความต่อกัน")
+print(1+9)
